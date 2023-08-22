8b9909b76e8baf9d4c9768fee7e74b971432031a
ChaBAYt69 <132995371+HaythemCH69@users.noreply.github.com>
2023-08-22 16:41:21 +0200

    Add files via upload
    API+base de données

--- app.py
+++ app.py
@@ -1,51 +1,52 @@
-from flask import Flask, request, current_app
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
 import requests
 import base64
-import re
-import configparser
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Charger les informations d'identification depuis le fichier de configuration
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-ADMIN_CREDENTIALS = f"{config['Credentials']['username']}:{config['Credentials']['password']}"
-OWNCLOUD_API_URL = "http://localhost:8080/ocs/v1.php/cloud/users"
-
-# Configurer la base de données
+# Configuration de la base de données SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-# Modèle pour la table des utilisateurs
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
 
+OWNCLOUD_API_URL = "http://localhost:8080/ocs/v1.php/cloud/users"
+ADMIN_CREDENTIALS = "admin:admin"
+
 @app.route('/register', methods=['POST'])
 def register_user():
-    # Obtenez les informations du formulaire
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # Vérifiez si l'utilisateur existe déjà dans la base de données
     if user_exists(username):
         return "L'utilisateur existe déjà."
 
-    # Enregistrez l'utilisateur dans la base de données
-    new_user = User(username=username)
-    db.session.add(new_user)
-    db.session.commit()
+    user_data = {
+        "userid": username,
+        "password": password,
+        "groups": ["Everyone"]
+    }
 
-    return "Utilisateur enregistré avec succès !"
+    headers = {
+        "Authorization": "Basic " + base64.b64encode(ADMIN_CREDENTIALS.encode()).decode()
+    }
 
-# Fonction pour vérifier si un utilisateur existe déjà dans la base de données
+    response = requests.post(OWNCLOUD_API_URL, data=user_data, headers=headers)
+
+    if response.ok:
+        new_user = User(username=username)
+        db.session.add(new_user)
+        db.session.commit()
+        return "Utilisateur enregistré avec succès !"
+    else:
+        return f"Échec de l'enregistrement de l'utilisateur. Code de statut : {response.status_code}"
+
 def user_exists(username):
-    user = User.query.filter_by(username=username).first()
-    return user is not None
+    return User.query.filter_by(username=username).first() is not None
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Créer les tables dans la base de données si elles n'existent pas déjà
+    db.create_all()  # Créer les tables dans la base de données si elles n'existent pas déjà
     app.run(debug=True, host='0.0.0.0', port=5000)
