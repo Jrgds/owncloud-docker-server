c4705c6ddcf078b38818d66f0ae1e9feb3c2cc52
ChaBAYt69 <132995371+HaythemCH69@users.noreply.github.com>
2023-08-24 17:24:35 +0200

    structure de répertoire
    structure de répertoire

--- app.py
+++ app.py
@@ -1,52 +1,19 @@
-from flask import Flask, request
-import requests
-import base64
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from modules.user_management import register_user
+from modules.sync import sync_users
+from modules.models import db
 
 app = Flask(__name__)
 
 # Configuration de la base de données SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-
-OWNCLOUD_API_URL = "http://localhost:8080/ocs/v1.php/cloud/users"
-ADMIN_CREDENTIALS = "admin:admin"
-
-@app.route('/register', methods=['POST'])
-def register_user():
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    if user_exists(username):
-        return "L'utilisateur existe déjà."
-
-    user_data = {
-        "userid": username,
-        "password": password,
-        "groups": ["Everyone"]
-    }
-
-    headers = {
-        "Authorization": "Basic " + base64.b64encode(ADMIN_CREDENTIALS.encode()).decode()
-    }
-
-    response = requests.post(OWNCLOUD_API_URL, data=user_data, headers=headers)
-
-    if response.ok:
-        new_user = User(username=username)
-        db.session.add(new_user)
-        db.session.commit()
-        return "Utilisateur enregistré avec succès !"
-    else:
-        return f"Échec de l'enregistrement de l'utilisateur. Code de statut : {response.status_code}"
-
-def user_exists(username):
-    return User.query.filter_by(username=username).first() is not None
+# Routes
+app.route('/register', methods=['POST'])(register_user)
+app.route('/sync', methods=['POST'])(sync_users)
 
 if __name__ == '__main__':
-    db.create_all()  # Créer les tables dans la base de données si elles n'existent pas déjà
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ config.py
@@ -0,0 +1,2 @@
+OWNCLOUD_API_URL = "http://localhost:8080/ocs/v1.php/cloud/users"
+ADMIN_CREDENTIALS = "admin:admin"
